42b52c9e664e9fb6da7712ab06263bc9a7005b80
Hari-2004 <70603381+Hari-2004@users.noreply.github.com>
2020-11-03 18:06:48 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Cow)
+    basic.showIcon(IconNames.Ghost)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
